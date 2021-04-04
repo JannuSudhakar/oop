37f78c3d0f246fbcf3195fff4ddf755ce79ac738
Jannu Sudhakar <sudhajannu95@gmail.com>
2021-04-05 04:20:14 +0530

    first commit

---
+++ chessboard.js
@@ -0,0 +1,46 @@
+//chessboard.js
+const prompt = require('prompt-sync')();
+
+function chessBoard(){
+  return {
+    position: [['R','N','B','Q','K','B','N','R'],
+               ['P','P','P','P','P','P','P','P'],
+               [' ',' ',' ',' ',' ',' ',' ',' '],
+               [' ',' ',' ',' ',' ',' ',' ',' '],
+               [' ',' ',' ',' ',' ',' ',' ',' '],
+               [' ',' ',' ',' ',' ',' ',' ',' '],
+               ['P','P','P','P','P','P','P','P'],
+               ['R','N','B','Q','K','B','N','R']],
+    move_history : [],
+    curr_turn : "white",
+    print_position : function(){
+      ret = "+---+---+---+---+---+---+---+---+\n";
+      for(var i = 0; i < 8; i++){
+        ret += "| ";
+        for(var j = 0; j < 8; j++){
+          ret += this.position[i][j];
+          ret += " | ";
+        }
+        ret += "\n+---+---+---+---+---+---+---+---+\n";
+      }
+      console.log(ret);
+    },
+    move : function(){
+      move_str = prompt(`enter move for ${this.curr_turn}: `);
+      this.position[1][this.move_history.length] = " ";
+      //replace this with something that makes sense
+      this.move_history.push(move_str);
+      if(this.curr_turn == "white"){
+        this.curr_turn = "black";
+      }
+      else{
+        this.curr_turn = "white";
+      }
+    },
+    finished : function(){
+      return(this.move_history.length >= 8);
+    }
+  }
+}
+
+module.exports = chessBoard;
---
+++ main.js
@@ -0,0 +1,10 @@
+chessBoard = require('./chessboard.js');
+
+board = chessBoard();
+console.log(board.finished());
+while(!board.finished()){
+  board.print_position();
+  board.move();
+}
+board.print_position();
+console.log(board.result);
